987c897265c66af0145bc0befa4c32ed37bd5b01
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-03-11 19:59:09 +0300

    + init.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
+import init
+import constants as c
+
+
 def main():
-	print("hello")
-	pass
-
-
+	display = init.get_display()
+	clock = init.get_clock()
+	game_states = init.get_game_states()
+	controller = init.get_controller()
+	controller.run_game(display, clock, game_states, c.STARTING_STATE)
+	
+	
 if __name__ == '__main__':
 	main()
